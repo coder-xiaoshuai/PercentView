f14cfca15bf63e9a201d45167a35bfe9b6404b3d
zhangshuai <zhangshuai1@miliantech.com>
2020-08-12 14:40:36 +0800

    改为androidx 优化view

--- app/src/main/java/com/example/xiaoshuai/percentview/WinRateView.java
+++ app/src/main/java/com/example/xiaoshuai/percentview/WinRateView.java
@@ -1,8 +1,5 @@
 package com.example.xiaoshuai.percentview;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -11,7 +8,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.renderscript.Sampler;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -21,8 +17,9 @@ import android.widget.Toast;
 
 /**
  * Created by xiaoshuai on 2016/12/18.
+ * 用于显示胜率的自定义view
  */
-public class PercentView extends View {
+public class WinRateView extends View {
     private static final String TAG = "PercentView";
 
     /*---------默认的三种情况的颜色-----------*/
@@ -35,23 +32,24 @@ public class PercentView extends View {
     /*-----------自定义属性的相关变量-----------*/
     private int mColorWin;
     private int mColorLose;
-    private int mColorDogfall;
+    private int mColorTie;
     private float mPercentWin;
     private float mPercentLose;
-    private float mPercentDogfall;
+    private float mPercentTie;
     private boolean isStartAnimator;
 
     private float angle_win_start;
     private float angle_win_end;
     private float angle_win_tmp;
-    private float angle_dogfall_start;
-    private float angle_dogfall_end;
-    private float angle_dogfall_tmp;
+    private float angle_tie_start;
+    private float angle_tie_end;
+    private float angle_tie_tmp;
     private float angle_lose_start;
     private float angle_lose_end;
     private float angle_lose_tmp;
+    private RectF rectF;
     private Rect rectWin;
-    private Rect rectDogfall;
+    private Rect rectTie;
     private Rect rectLose;
 
     private Paint mPaint;
@@ -59,27 +57,27 @@ public class PercentView extends View {
     private static final int DEFAULT_SIZE = 200;//默认尺寸
 
 
-    public PercentView(Context context) {
+    public WinRateView(Context context) {
         this(context, null);
     }
 
-    public PercentView(Context context, AttributeSet attrs) {
+    public WinRateView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public PercentView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public WinRateView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.PercentView);
-        mColorWin = array.getColor(R.styleable.PercentView_color_win, DEFAULT_COLOR_WIN);
-        mColorLose = array.getColor(R.styleable.PercentView_color_lose, DEFAULT_COLOR_LOSE);
-        mColorDogfall = array.getColor(R.styleable.PercentView_color_dogfall, DEFAULT_COLOR_DOGFALL);
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.WinRateView);
+        mColorWin = array.getColor(R.styleable.WinRateView_color_win, DEFAULT_COLOR_WIN);
+        mColorLose = array.getColor(R.styleable.WinRateView_color_lose, DEFAULT_COLOR_LOSE);
+        mColorTie = array.getColor(R.styleable.WinRateView_color_tie, DEFAULT_COLOR_DOGFALL);
 
-        mPercentWin = array.getFloat(R.styleable.PercentView_win_percent, DEFAULT_PERCENT);
-        mPercentLose = array.getFloat(R.styleable.PercentView_lose_percent, DEFAULT_PERCENT);
-        mPercentDogfall = array.getFloat(R.styleable.PercentView_dogfall_percent, DEFAULT_PERCENT);
+        mPercentWin = array.getFloat(R.styleable.WinRateView_win_percent, DEFAULT_PERCENT);
+        mPercentLose = array.getFloat(R.styleable.WinRateView_lose_percent, DEFAULT_PERCENT);
+        mPercentTie = array.getFloat(R.styleable.WinRateView_tie_percent, DEFAULT_PERCENT);
 
-        isStartAnimator = array.getBoolean(R.styleable.PercentView_is_startAnimator, false);
+        isStartAnimator = array.getBoolean(R.styleable.WinRateView_is_startAnimator, false);
 
         checkIsLegal();
         calculateEachAngle();
@@ -105,15 +103,13 @@ public class PercentView extends View {
      * 检查是否合法
      */
     private void checkIsLegal() {
-        if ((mPercentWin + mPercentLose + mPercentDogfall) != 100) {
+        if ((mPercentWin + mPercentLose + mPercentTie) != 100) {
             try {
                 throw new Exception("三种情况之和必须是一百");
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            mPercentWin = mPercentLose = mPercentDogfall = DEFAULT_PERCENT;
-
-
+            mPercentWin = mPercentLose = mPercentTie = DEFAULT_PERCENT;
         }
     }
 
@@ -127,51 +123,52 @@ public class PercentView extends View {
         angle_win_end = mPercentWin == 0 ? 0 : 360 * mPercentWin / 100 - 2;
 
 
-        angle_dogfall_start = (270 + 360 * mPercentWin / 100) % 360 + 2;
-        angle_dogfall_tmp = 0;
-        angle_dogfall_end = mPercentDogfall == 0 ? 0 : 360 * mPercentDogfall / 100 - 2;
+        angle_tie_start = (270 + 360 * mPercentWin / 100) % 360 + 2;
+        angle_tie_tmp = 0;
+        angle_tie_end = mPercentTie == 0 ? 0 : 360 * mPercentTie / 100 - 2;
 
-        angle_lose_start = (270 + 360 * (mPercentWin / 100 + mPercentDogfall / 100)) % 360 + 2;
+        angle_lose_start = (270 + 360 * (mPercentWin / 100 + mPercentTie / 100)) % 360 + 2;
         angle_lose_tmp = 0;
         angle_lose_end = mPercentLose == 0 ? 0 : 360 * mPercentLose / 100 - 2;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
         super.onDraw(canvas);
         /*------------先绘制外层弧线------------*/
-        mPaint.setStrokeWidth(10);
+        float lineWidth = 20;
+        mPaint.setStrokeWidth(20);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
 
-        RectF rect = new RectF(getPaddingLeft() + 5, getPaddingTop() + 5, getWidth() - 2 * getPaddingLeft() - 5, getHeight() - 2 * getPaddingTop() - 5);
+        if (rectF == null) {
+            rectF = new RectF(getPaddingLeft() + lineWidth / 2, getPaddingTop() + lineWidth / 2, getWidth() - 2 * getPaddingLeft() - lineWidth / 2, getHeight() - 2 * getPaddingTop() - lineWidth / 2);
+        }
         //起始角度为270度
         if (isStartAnimator) {
             mPaint.setColor(mColorWin);
-            canvas.drawArc(rect, angle_win_start, angle_win_tmp, false, mPaint);
-            mPaint.setColor(mColorDogfall);
-            canvas.drawArc(rect, angle_dogfall_start, angle_dogfall_tmp, false, mPaint);
+            canvas.drawArc(rectF, angle_win_start, angle_win_tmp, false, mPaint);
+            mPaint.setColor(mColorTie);
+            canvas.drawArc(rectF, angle_tie_start, angle_tie_tmp, false, mPaint);
             mPaint.setColor(mColorLose);
-            canvas.drawArc(rect, angle_lose_start, angle_lose_tmp, false, mPaint);
+            canvas.drawArc(rectF, angle_lose_start, angle_lose_tmp, false, mPaint);
             Log.d(TAG, "-----angle_win_tmp" + angle_win_tmp);
         } else {
             mPaint.setColor(mColorWin);
-            canvas.drawArc(rect, angle_win_start, angle_win_end, false, mPaint);
-            mPaint.setColor(mColorDogfall);
-            canvas.drawArc(rect, angle_dogfall_start, angle_dogfall_end, false, mPaint);
+            canvas.drawArc(rectF, angle_win_start, angle_win_end, false, mPaint);
+            mPaint.setColor(mColorTie);
+            canvas.drawArc(rectF, angle_tie_start, angle_tie_end, false, mPaint);
             mPaint.setColor(mColorLose);
-            canvas.drawArc(rect, angle_lose_start, angle_lose_end, false, mPaint);
+            canvas.drawArc(rectF, angle_lose_start, angle_lose_end, false, mPaint);
         }
 
         /*------------画矩形和文字------------*/
 
         //因为需要让这些内容居中  我们需要先画平局的  以它为基准来画别的
-        int textsize = 40;
-
-        mPaint.setTextSize(textsize);
+        int textSize = 36;
+        mPaint.setTextSize(textSize);
         Paint.FontMetrics fm = mPaint.getFontMetrics();
-        int textheight = (int) Math.ceil(fm.descent - fm.ascent);
+        int textHeight = (int) Math.ceil(fm.descent - fm.ascent);
 
         mPaint.setStyle(Paint.Style.FILL);
         float textLength = mPaint.measureText("胜利60%");
@@ -180,23 +177,29 @@ public class PercentView extends View {
         //矩形与文字之间的间隙
         int text_padding = 10;
         int space_raw = 80;
-        mPaint.setColor(mColorDogfall);
-        rectDogfall = new Rect((getWidth() - (int) textLength - side_size - text_padding) / 2, getHeight() / 2 - textheight / 2, (getWidth() - (int) textLength - side_size - text_padding) / 2 + side_size + text_padding + (int) textLength, getHeight() / 2 - textheight / 2 + textheight);
+        mPaint.setColor(mColorTie);
+        if (rectTie == null) {
+            rectTie = new Rect((getWidth() - (int) textLength - side_size - text_padding) / 2, getHeight() / 2 - textHeight / 2, (getWidth() - (int) textLength - side_size - text_padding) / 2 + side_size + text_padding + (int) textLength, getHeight() / 2 - textHeight / 2 + textHeight);
+        }
         canvas.drawRect((getWidth() - textLength - side_size - text_padding) / 2, getHeight() / 2 - side_size / 2, (getWidth() - textLength - side_size - text_padding) / 2 + side_size, getHeight() / 2 - side_size / 2 + side_size, mPaint);
         mPaint.setColor(Color.BLACK);
-        canvas.drawText("平局" + (int) mPercentDogfall + "%", (getWidth() - textLength - side_size) / 2 + side_size + text_padding, getHeight() / 2 + textheight / 2, mPaint);
+        canvas.drawText("平局" + (int) mPercentTie + "%", (getWidth() - textLength - side_size) / 2 + side_size + text_padding, getHeight() / 2 + textHeight / 2, mPaint);
 
         mPaint.setColor(mColorWin);
-        rectWin = new Rect((getWidth() - (int) textLength - side_size - text_padding) / 2, getHeight() / 2 - textheight / 2 - space_raw, (getWidth() - (int) textLength - side_size - text_padding) / 2 + side_size + text_padding + (int) textLength, getHeight() / 2 - textheight / 2 - space_raw + textheight);
+        if (rectWin == null) {
+            rectWin = new Rect((getWidth() - (int) textLength - side_size - text_padding) / 2, getHeight() / 2 - textHeight / 2 - space_raw, (getWidth() - (int) textLength - side_size - text_padding) / 2 + side_size + text_padding + (int) textLength, getHeight() / 2 - textHeight / 2 - space_raw + textHeight);
+        }
         canvas.drawRect((getWidth() - textLength - side_size - text_padding) / 2, getHeight() / 2 - side_size / 2 - space_raw, (getWidth() - textLength - side_size - text_padding) / 2 + side_size, getHeight() / 2 - side_size / 2 - space_raw + side_size, mPaint);
         mPaint.setColor(Color.BLACK);
-        canvas.drawText("胜利" + (int) mPercentWin + "%", (getWidth() - textLength - side_size) / 2 + side_size + text_padding, getHeight() / 2 + textheight / 2 - space_raw, mPaint);
+        canvas.drawText("胜利" + (int) mPercentWin + "%", (getWidth() - textLength - side_size) / 2 + side_size + text_padding, getHeight() / 2 + textHeight / 2 - space_raw, mPaint);
 
         mPaint.setColor(mColorLose);
-        rectLose = new Rect((getWidth() - (int) textLength - side_size - text_padding) / 2, getHeight() / 2 - textheight / 2 + space_raw, (getWidth() - (int) textLength - side_size - text_padding) / 2 + side_size + text_padding + (int) textLength, getHeight() / 2 - textheight / 2 + textheight + space_raw);
+        if (rectLose == null) {
+            rectLose = new Rect((getWidth() - (int) textLength - side_size - text_padding) / 2, getHeight() / 2 - textHeight / 2 + space_raw, (getWidth() - (int) textLength - side_size - text_padding) / 2 + side_size + text_padding + (int) textLength, getHeight() / 2 - textHeight / 2 + textHeight + space_raw);
+        }
         canvas.drawRect((getWidth() - textLength - side_size - text_padding) / 2, getHeight() / 2 - side_size / 2 + side_size / 2 + space_raw, (getWidth() - textLength - side_size - text_padding) / 2 + side_size, getHeight() / 2 - side_size / 2 + side_size / 2 + space_raw + side_size, mPaint);
         mPaint.setColor(Color.BLACK);
-        canvas.drawText("失败" + (int) mPercentLose + "%", (getWidth() - textLength - side_size) / 2 + side_size + text_padding, getHeight() / 2 + textheight / 2 + space_raw, mPaint);
+        canvas.drawText("失败" + (int) mPercentLose + "%", (getWidth() - textLength - side_size) / 2 + side_size + text_padding, getHeight() / 2 + textHeight / 2 + space_raw, mPaint);
 
     }
 
@@ -210,8 +213,6 @@ public class PercentView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-
         int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
@@ -225,8 +226,8 @@ public class PercentView extends View {
         } else if (heightMeasureSpec == MeasureSpec.AT_MOST) {
             setMeasuredDimension(DEFAULT_SIZE, DEFAULT_SIZE);
         } else {
-            int c = Math.min(widthSpecSize, heightSpecSize);
-            setMeasuredDimension(c, c);
+            int minSize = Math.min(widthSpecSize, heightSpecSize);
+            setMeasuredDimension(minSize, minSize);
         }
 
     }
@@ -235,12 +236,12 @@ public class PercentView extends View {
      * 设置各种情况的百分比
      *
      * @param percentWin
-     * @param percentDogfall
+     * @param percentTie
      * @param percentLose
      */
-    public void setEachPercent(float percentWin, float percentDogfall, float percentLose) {
+    public void setEachPercent(float percentWin, float percentTie, float percentLose) {
         this.mPercentWin = percentWin;
-        this.mPercentDogfall = percentDogfall;
+        this.mPercentTie = percentTie;
         this.mPercentLose = percentLose;
         //需要重新检验和计算
         checkIsLegal();
@@ -254,14 +255,14 @@ public class PercentView extends View {
      */
     private void startAnimation() {
         ValueAnimator animator_win = ValueAnimator.ofFloat(0, angle_win_end);
-        animator_win.setDuration(1000 * 5);
+        animator_win.setDuration(500);
         animator_win.setInterpolator(new LinearInterpolator());
         animator_win.start();
-        final ValueAnimator animator_dogfall = ValueAnimator.ofFloat(0, angle_dogfall_end);
-        animator_dogfall.setDuration(1000 * 5);
+        final ValueAnimator animator_dogfall = ValueAnimator.ofFloat(0, angle_tie_end);
+        animator_dogfall.setDuration(500);
         animator_dogfall.setInterpolator(new LinearInterpolator());
         final ValueAnimator animator_lose = ValueAnimator.ofFloat(0, angle_lose_end);
-        animator_lose.setDuration(1000 * 5);
+        animator_lose.setDuration(500);
         animator_lose.setInterpolator(new LinearInterpolator());
 
         animator_win.start();
@@ -286,7 +287,7 @@ public class PercentView extends View {
         animator_dogfall.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                angle_dogfall_tmp = (float) valueAnimator.getAnimatedValue();
+                angle_tie_tmp = (float) valueAnimator.getAnimatedValue();
                 invalidate();
             }
         });
@@ -306,13 +307,12 @@ public class PercentView extends View {
         int currentY = (int) event.getY();
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             //在这里可以根据自己的需求向外部暴露自己的接口
-
             if (rectWin.contains(currentX, currentY)) {
-                Toast.makeText(this.getContext(), "胜利的比率", Toast.LENGTH_SHORT).show();
-            } else if (rectDogfall.contains(currentX, currentY)) {
-                Toast.makeText(this.getContext(), "平局的比率", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this.getContext(), "胜率" + mPercentWin + "%", Toast.LENGTH_SHORT).show();
+            } else if (rectTie.contains(currentX, currentY)) {
+                Toast.makeText(this.getContext(), "平局率" + mPercentTie + "%", Toast.LENGTH_SHORT).show();
             } else if (rectLose.contains(currentX, currentY)) {
-                Toast.makeText(this.getContext(), "失败的比率", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this.getContext(), "失败率" + mPercentLose + "%", Toast.LENGTH_SHORT).show();
             }
             return true;
         }
